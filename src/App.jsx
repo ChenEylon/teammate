b0e11141d4bcfe5a76055cebe33a771869b4dc3f
amitbarha <amitbar131@gmail.com>
2023-05-28 12:26:14 +0300

    add const

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 const [board,setboard]=useState(Array(9).fill(null))
 const [X, isX] = useState(true)
 const handleclick=(idx)=>{
+  const hello = 5;
   const newboard= board.map((value,index)=>{
     if (index===idx) { return value===X
     } else {return value}
