b30c66f5167caf29dcc5a7827a5e60a0e95631ac
ChenEylon <cheneylon1@gmail.com>
2023-05-29 15:37:16 +0300

    new 2

--- src/components/winner.jsx
+++ src/components/winner.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-export const Winner = ({ score }) => {
+export const Winner = ({ score ,gameEnded}) => {
 
     return (
         <div>
             {score ? <div>The winner is {score}</div> : null}
+            {gameEnded ? <div>there is no winner</div> : null}
         </div>
     )
 }
